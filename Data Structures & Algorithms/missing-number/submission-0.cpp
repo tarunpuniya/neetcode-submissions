ed8b78224ed0c2028b715398c5426bec169335c8
tarunpuniya <tarunpuniya287@gmail.com>
2026-09-22 22:03:18 +0530

    Add: missing-number - submission-0

---
+++ Data Structures & Algorithms/missing-number/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+
+        int expected = n * (n + 1) / 2;
+        int actual = 0;
+
+        for(int i=0; i<n; i++) {
+            actual += nums[i];
+        }
+        return expected - actual;
+    }
+};
