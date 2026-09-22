6d8c62efd318b3254eef1e04d2f8ded56e0eaeba
tarunpuniya <tarunpuniya287@gmail.com>
2026-09-22 21:51:19 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/single-number/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map<int,int>freq;
+
+        for(int i=0; i<n; i++){
+            freq[nums[i]]++;
+        }    
+
+        for(auto it:freq){
+            if(it.second==1) return it.first;
+        }
+        return -1;
+    }
+};
